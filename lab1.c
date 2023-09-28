438e1012359bd726d688e6a9bcf3b6d48e595c54
hunterlillie456 <hunterlillie456@gmail.com>
2023-09-28 12:44:08 -0400

    testing

--- lab1.c
+++ lab1.c
@@ -2,4 +2,18 @@
  * Implement your solution in thi file
  *
  */
+#include <stdio.h>
+
+char* readString(char* fileName){
+    FILE * ex = fopen("ex.txt","w");
+    fprintf(ex,mysteryExplode("abc"));
+    char line [100];
+    fgets(line,100,ex);
+    fileName = line;
+    return fileName;
+}
+
+char* mysteryExplode(const char* str){
+
+}
 
