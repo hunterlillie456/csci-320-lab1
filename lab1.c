f2f6913a8372dcb7f69e999304d336afb5166104
hunterlillie456 <hunterlillie456@gmail.com>
2023-10-06 17:49:53 -0400

    submission 1

--- lab1.c
+++ lab1.c
@@ -3,17 +3,23 @@
  *
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lab1.h"
 
 char* readString(char* fileName){
-    FILE * ex = fopen("ex.txt","w");
-    fprintf(ex,mysteryExplode("abc"));
-    char line [100];
+    FILE * ex = fopen(fileName,"r");
+    char* line = (char *)malloc(MAX_LINE_LEN*sizeof(char));
     fgets(line,100,ex);
-    fileName = line;
-    return fileName;
+    return line;
 }
+
 
 char* mysteryExplode(const char* str){
-
+    char* newString = (char *)malloc(((strlen(str)*(strlen(str)+1))/2)+1);
+  	
+    for (int i=0; i<=strlen(str)-1;i++){
+        strncat(newString, str, i);
+    }
+    return newString;
 }
-
